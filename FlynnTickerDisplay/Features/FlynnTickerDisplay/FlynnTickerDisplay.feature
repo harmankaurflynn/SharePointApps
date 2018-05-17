ba6f2856076bcea2562b60b78818ff6f4921d534
harman.kaur <harman.kaur@IT00007068>
2018-05-17 11:28:54 -0400

    Add project files.

---
+++ FlynnTickerDisplay/Features/FlynnTickerDisplay/FlynnTickerDisplay.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="1d37b281-ab81-4923-95e3-f1a1bdabe147" description="List and other features to display ticker on page i.e title and count" featureId="1d37b281-ab81-4923-95e3-f1a1bdabe147" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Flynn Ticker Display" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="0609dad0-0cbd-4cc1-9d45-d87508eeeed8" />
+    <projectItemReference itemId="3d656e81-6935-4d58-9ea1-08a6a485126f" />
+    <projectItemReference itemId="48bc519d-2cfd-4ac1-b013-9162035f0d07" />
+    <projectItemReference itemId="c619a7ca-f48c-400c-93f4-bdeda269224b" />
+  </projectItems>
+</feature>
---
+++ FlynnTickerDisplay/Scripts/App.js
@@ -0,0 +1,90 @@
+﻿'use strict';
+var hostweburl;
+var appweburl;
+var listname;
+
+
+var countTickerApp = angular.module('countTickerApp', []);
+countTickerApp.controller('CountTickerController',
+    function ($scope, $http) {
+        hostweburl = decodeURIComponent($.getUrlVar("SPHostUrl"));
+        appweburl = decodeURIComponent($.getUrlVar("SPAppWebUrl"));
+        listname = decodeURIComponent($.getUrlVar("ListName"));
+        
+        $scope.countTickers = [];
+        $scope.errormessage;
+        $scope.erroroccurred = true;
+        if (listname && listname != undefined && listname != "undefined") {
+            $scope.erroroccurred = false;
+            var scriptbase = hostweburl + "/_layouts/15/";
+            $.getScript(scriptbase + "SP.Runtime.js",
+                function () {
+                    $.getScript(scriptbase + "SP.js",
+                        function () {
+                            $.getScript(scriptbase + "SP.RequestExecutor.js", function () {
+                                var context = new SP.ClientContext(appweburl);
+                                var factory = new SP.ProxyWebRequestExecutorFactory(appweburl);
+                                context.set_webRequestExecutorFactory(factory);
+                                var appContextSite = new SP.AppContextSite(context, hostweburl);
+                                var web = appContextSite.get_web();
+                                context.load(web);
+
+                                var list = web.get_lists().getByTitle(listname);
+                                var camlQuery = SP.CamlQuery.createAllItemsQuery();
+                                this.listItems = list.getItems(camlQuery);
+                                context.load(this.listItems);
+
+                                context.executeQueryAsync(
+                                    Function.createDelegate(this, function () {
+                                       $scope.erroroccurred = false;
+                                       
+                                        var ListEnumerator = this.listItems.getEnumerator();
+                                        while (ListEnumerator.moveNext()) {
+                                            var currentItem = ListEnumerator.get_current();
+                                            $scope.countTickers.push({
+                                                title: currentItem.get_item('Title'),
+                                                count: currentItem.get_item('Count')
+                                            });
+
+
+                                            $scope.$apply();
+
+                                        }
+                                    }),
+                                    Function.createDelegate(this, function (sender, args) {
+                                        $scope.errormessage = "An error occurred";
+                                        $scope.erroroccurred = true;
+                                    })
+                                );
+
+                            });
+                        }
+                    );
+                }
+            );
+        }
+        else
+        {
+            $scope.errormessage = "Select ListName in webpart properties";
+            
+        }
+    }
+);
+jQuery.extend({
+    getUrlVars: function () {
+        var vars = [],
+            hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    },
+    getUrlVar: function (name) {
+        return jQuery.getUrlVars()[name];
+    }
+});
+
+
